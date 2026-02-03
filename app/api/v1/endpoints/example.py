cc1a2214e48a3054aae97d2d610f19ea287771e0
juyoung <wanke94@naver.com>
2026-02-03 14:30:17 +0900

    필요없는 파일 제거 및 gitignore, requirements 추가

--- app/api/v1/endpoints/example.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.schemas.common import Message
-
-router = APIRouter()
-
-@router.get("/hello", response_model=Message)
-def hello_world():
-    return {"message": "Hello from API v1"}
